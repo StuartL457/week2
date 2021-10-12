658f6f469ace3f18591748787b6ac95aa5addae2
StuartL457 <stuart.lewis95@gmail.com>
2021-10-12 14:59:35 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import Product from './Product';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+function clickHandeler() {
+  window.alert(
+    toString(
+      <Product name="apple" id="123" datePurchased="12/10/21" price="£0.50" />
+    )
+  );
+}
+
 function App() {
   return (
     <div>
-      <Product name="apple" id="123" datePurchaced="12/10/21" price="£0.50" />
+      <Product name="apple" id="123" datePurchased="12/10/21" price="£0.50" />
+
+      <button onClick={clickHandeler}>Click Me!</button>
     </div>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -1,9 +1,12 @@
+import React from 'react';
+
 function Product(props) {
   return (
     <div>
-      Product name = {props.name} <br></br>Product id = {props.id};<br></br>
-      Product datePurchased = {props.datePurchased};<br></br>Product price ={' '}
-      {props.price};
+      name = {props.name} 
+      <br></br>id = {props.id}
+      <br></br>datePurchased = {props.datePurchased}
+      <br></br>price = {props.price}
     </div>
   );
 }
